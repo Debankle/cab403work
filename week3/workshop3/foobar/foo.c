af24eb4ebe948fff368adaaa159265ef86466f0a
Oliver Pinel <oliver.debankle@gmail.com>
2024-08-06 18:40:37 +1000

    lecture week 3

---
+++ week3/workshop3/foobar/foo.c
@@ -0,0 +1,8 @@
+#include "foo.h"
+
+#include <stdio.h>
+void printHelloMake(void) {
+    printf("\nHello CAB403 - Welcome to makefiles\n");
+    printf("\nWelcome to Practical 4\n");
+    return;
+}
---
+++ week3/workshop3/hellomake/hellofunc.c
@@ -0,0 +1,7 @@
+#include <hellomake.h>
+#include <stdio.h>
+void printHelloMake(void) {
+    printf("\nHello CAB403 - Welcome to makefiles\n");
+    printf("\nWelcome to Practical 3\n");
+    return;
+}
